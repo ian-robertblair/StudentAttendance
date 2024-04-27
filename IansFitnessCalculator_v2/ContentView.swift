8881de34427d7d2a5a0620707e76208ade8c3cd8
ian-robertblair <ianrobertblair@icloud.com>
2024-04-28 06:46:52 +0800

    Initial Commit

---
+++ IansFitnessCalculator_v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IansFitnessCalculator_v2
+//
+//  Created by ian robert blair on 2024/4/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IansFitnessCalculator_v2/IansFitnessCalculator_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  IansFitnessCalculator_v2App.swift
+//  IansFitnessCalculator_v2
+//
+//  Created by ian robert blair on 2024/4/28.
+//
+
+import SwiftUI
+
+@main
+struct IansFitnessCalculator_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
